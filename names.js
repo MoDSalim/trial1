7bc3001561a84c97d2451dacc1b57540fb90afc1
Broadsword81 <60960535+Broadsword81@users.noreply.github.com>
2022-07-24 17:15:17 +0530

    first commit

---
+++ names.js
@@ -0,0 +1,6 @@
+const first = `Brook`;
+const second = `Momonuske`;
+const third = `Yamato`;
+const fourth = `jinbei`;
+
+module.exports = { first, second, third, fourth };
---
+++ utils.js
@@ -0,0 +1,18 @@
+// const newNakama = (name) => {
+//   console.log(`The new Nakama is ${name}`);
+// };
+
+// module.exports.items = {
+//   name: "Salim",
+//   class: `10th std`,
+//   marks: `Awesome`,
+// };
+
+// module.exports.func = newNakama;
+
+const best = `Salim`;
+const sayHello = (name) => {
+  console.log(`hello ${name}`);
+};
+
+module.exports = { sayHello, best };
